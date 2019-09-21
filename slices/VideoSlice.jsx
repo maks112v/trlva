f27ca62f5439639ed020808b6431544ce6575e2c
Maks <maks112v@gmail.com>
2019-09-21 14:25:57 +0300

    Update VideoSlice.jsx

--- slices/VideoSlice.jsx
+++ slices/VideoSlice.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { RichText } from "prismic-reactjs"
-import { colors } from "../components/Styles"
+import { colors, breakingPoints } from "../components/Styles"
 import { FiPlay } from "react-icons/fi"
 import FsLightbox from "fslightbox-react"
 
@@ -23,26 +23,45 @@ export default function VideoSlice({ primary, items }) {
           boxShadow: "0 15px 38px 10px rgba(0,0,0,0.1 )",
           backgroundSize: "cover",
           position: "relative",
+          [breakingPoints.lg]: {
+            paddingTop: "75%",
+          },
+          [breakingPoints.sm]: {
+            paddingTop: "100%",
+          },
         }}>
-        <button
-          onClick={() => setToggler(!toggler)}
+        <div
           css={{
             position: "absolute",
-            top: "50%",
-            left: "46%",
-            backgroundColor: colors.accent,
-            height: 75,
-            width: 75,
-            borderRadius: "50%",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            outline: "none",
-            border: "none",
-            cursor: "pointer",
+            [breakingPoints.sm]: {
+              bottom: 50,
+              alignItems: "flex-end",
+            },
           }}>
-          <FiPlay color={colors.text.dark} size="25px" />
-        </button>
+          <button
+            onClick={() => setToggler(!toggler)}
+            css={{
+              backgroundColor: colors.accent,
+              height: 75,
+              width: 75,
+              borderRadius: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              outline: "none",
+              border: "none",
+              cursor: "pointer",
+            }}>
+            <FiPlay color={colors.text.dark} size="25px" />
+          </button>
+        </div>
         <div css={{ position: "absolute", top: 75, left: 20 }}>
           <div
             css={{
@@ -61,6 +80,17 @@ export default function VideoSlice({ primary, items }) {
                 fontWeight: 300,
                 fontSize: 20,
               },
+              [breakingPoints.md]: {
+                order: 2,
+                marginLeft: 0,
+                marginTop: 0,
+                h3: {
+                  fontSize: 25,
+                },
+                p: {
+                  fontSize: 20,
+                },
+              },
             }}>
             <h2
               css={{
@@ -71,6 +101,11 @@ export default function VideoSlice({ primary, items }) {
                 fontSize: 120,
                 color: colors.text.light,
                 opacity: 0.1,
+                [breakingPoints.md]: {
+                  fontSize: 50,
+                  marginTop: -30,
+                  marginLeft: -10,
+                },
               }}>
               {RichText.asText(primary.accent_text)}
             </h2>
