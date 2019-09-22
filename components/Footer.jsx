31ac628ee729ee409466be5b725ced086c042c8f
Maks <maks112v@gmail.com>
2019-09-22 21:46:44 +0300

    footer grid

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,6 +7,7 @@ export default function Footer() {
         backgroundColor: colors.dark,
         position: "relative",
         paddingTop: 150,
+        background: `url(/static/grid.svg), ${colors.dark}`,
       }}>
       <img
         src="/static/bottom_accent.svg"
@@ -23,8 +24,33 @@ export default function Footer() {
           maxWidth: 1240,
           padding: "20px 20px",
           margin: "0px auto",
+          color: "white",
         }}>
-        <h1>Footer</h1>
+        <div
+          css={{
+            display: "flex",
+            justifyContent: "space-between",
+            ">div": {
+              width: "24%",
+            },
+          }}>
+          <div>
+            <h1>Logo</h1>
+            <p>
+              The day after the conference, continue learning with one of our
+              expert trainers. Places are limited – first come, first served!
+            </p>
+          </div>
+          <div>
+            <h3>Important Links</h3>
+          </div>
+          <div>
+            <h3>Learn More</h3>
+          </div>
+          <div>
+            <h3>Find Us</h3>
+          </div>
+        </div>
       </div>
     </footer>
   )
